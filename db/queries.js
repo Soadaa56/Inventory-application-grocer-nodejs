bb99ff93bb91aced832b8b4044c05c2904789d0c
Kevin Duffy <soadhlh@gmail.com>
2025-05-17 19:10:05 -0400

    Db query for individual products

--- db/queries.js
+++ db/queries.js
@@ -9,6 +9,17 @@ async function getAllProducts() {
   return rows
 }
 
+async function getProductWithId(productId) {
+  const result = await pool.query(`
+    SELECT products.*, categories.name AS category_name, suppliers.name AS supplier_name
+    FROM products
+    LEFT JOIN categories ON products.category_id=categories.id
+    LEFT JOIN suppliers ON products.supplier_id=suppliers.id;
+    WHERE products.id = $1
+    `, [productId])
+}
+
 module.exports = {
-  getAllProducts
+  getAllProducts,
+  getProductWithId
 }
