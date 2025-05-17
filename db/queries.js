caa5b21e5841f54ea603b105421681885c20290d
Kevin Duffy <soadhlh@gmail.com>
2025-05-17 18:08:24 -0400

    AllProducts query includes category name

--- db/queries.js
+++ db/queries.js
@@ -1,7 +1,11 @@
 const pool = require("./pool")
 
 async function getAllProducts() {
-  const { rows } = await pool.query("SELECT * FROM products")
+  const { rows } = await pool.query(`
+    SELECT products.*, categories.name AS category_name
+    FROM products
+    LEFT JOIN categories ON products.category_id=categories.id;
+    `)
   return rows
 }
 
