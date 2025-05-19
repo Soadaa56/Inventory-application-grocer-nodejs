4c1ac69b8ff015591f0445680c68b5933941fbdd
Kevin Duffy <soadhlh@gmail.com>
2025-05-19 14:20:05 -0400

    Refactor functions with express async handler middleware

--- controllers/suppliersController.js
+++ controllers/suppliersController.js
@@ -1,39 +1,41 @@
 const db = require("../db/queries")
+const expressAsyncHandler = require("express-async-handler")
 
-async function getAllSuppliers(req, res) {
+const getAllSuppliers = expressAsyncHandler(async (req, res) => {
   const suppliers = await db.getAllSuppliers()
 
   res.status(200).render("suppliers/index", {
     title: "Suppliers",
     suppliers: suppliers
   })
-}
+})
 
-async function getNewSupplier(req, res) {
+const getNewSupplier = expressAsyncHandler(async (req, res) => {
   res.render("suppliers/new", {
     title: "Add New Supplier"
-  })  
-}
+  })
+})
 
-async function postNewSupplier(req, res) {
+const postNewSupplier = expressAsyncHandler(async (req, res) => {
   const body = req.body
   const { supplierName } = body
+  console.log(body)
+  console.log(supplierName)
 
   await db.insertNewSupplier(supplierName)
-  res.status(200).redirect("suppliers/index", {})
-}
+  res.redirect("suppliers/index", {})
+})
 
-async function getProductsWithSupplier(req, res) {
+const getProductsWithSupplier = expressAsyncHandler(async (req, res) => {
   const { supplier } = req.params
   console.log(supplier)
   const productsBySupplier = await db.getProductsWithSupplier(supplier)
-  
-  console.log(productsBySupplier)
+
   res.status(200).render("suppliers/show", {
     title: productsBySupplier[0].supplier_name,
-    productsBySupplier : productsBySupplier 
+    productsBySupplier: productsBySupplier
   })
-}
+})
 
 module.exports = {
   getAllSuppliers,
